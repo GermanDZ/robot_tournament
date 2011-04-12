fd24fa2be02bf7f61d6881dc0f4702b1c6311278
GermanDZ <germ@ndz.com.ar>
2011-04-12 19:51:51 +0200

    Fix the step, probably a change on row definition.

--- features/step_definitions/gui_steps.rb
+++ features/step_definitions/gui_steps.rb
@@ -4,7 +4,7 @@ end
 
 Then /^I should see the following players listed:$/ do |table|
   table.raw.each do |row|
-    page.should have_content(row.to_s)
+    page.should have_content(row[0].to_s)
   end
 end
 
